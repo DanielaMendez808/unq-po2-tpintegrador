d792dc8b45c141aabcc2b259d806d9d731ee6632
Daniela Mendez <danieladelcarmenmendez@gmail.com>
2026-06-18 03:38:08 -0300

    implementado el preciofinal de pedido

--- src/ecommerce/Paquete.java
+++ src/ecommerce/Paquete.java
@@ -3,25 +3,33 @@ package ecommerce;
 import java.util.ArrayList;
 
 public class Paquete implements Item{
-	String Nombre;
-	ArrayList <Producto> productos = new ArrayList <>();
-	double Descuento;
-	double PrecioFinal;
-	public void agregarProductoAPaquete() {
-		
+	String nombre;
+	ArrayList <Item> items = new ArrayList <>();
+	double descuento;
+	public void agregarItemAPaquete(Item item) {
+		items.add(item);
 	}
-	public void eliminarProductoDePaquete() {
-		
+	public void eliminarItemDePaquete(Item item) {
+		items.remove(item);
 	}
 	public String nombre() {
-		return Nombre;
+		return nombre;
 	}
 	public String descripcion() {
 		return "No se que poner aca, consultar";
 	}
-	public double precioBaseCalculado();
+	public double precioDePaquete() {
 		//sumar el precio de todos los productos que lo componen
+		double precioTemporal = 0;
+		int iterador = 0;
+		while (iterador != items.size()) {
+			precioTemporal = precioTemporal + items.get(iterador).precioBaseCalculado();
+			iterador = iterador + 1;
+		}
+		return precioTemporal;
 	}
-	public double precioFinalCalculado();
-		//aplicar descuento de paquete a la suma de todos los precios
+	public double precioBaseCalculado(){
+		return this.precioDePaquete()* (1-this.descuento);
+			//aplicar descuento de paquete a la suma de todos los precios
+	}
 }
