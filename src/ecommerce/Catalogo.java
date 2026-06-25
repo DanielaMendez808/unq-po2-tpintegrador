9f45fc8bc080c0b108b7fb1ac027c33c65c9a7ca
Daniela Mendez <danieladelcarmenmendez@gmail.com>
2026-06-25 01:38:48 -0300

    metodos de agregar y quitar elementos de catalogo

--- src/ecommerce/Catalogo.java
+++ src/ecommerce/Catalogo.java
@@ -1,5 +1,29 @@
 package ecommerce;
 
-public class Catalogo {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Catalogo {
+	private List <Item> itemsDeCatalogo = new ArrayList<>();
+	public void agregarItemACatalogo(Item item) {
+		this.verificarQueElItemNoEsteEnElCatalogo(item);
+		itemsDeCatalogo.add(item);
+	}
+	public void verificarQueElItemNoEsteEnElCatalogo(Item item) {
+		if(this.itemEstaEnElCatalogo(item)) {
+			throw new RuntimeException("El item"+item.nombre()+"ya existe en el catalogo");
+		}
+	}
+	public boolean itemEstaEnElCatalogo(Item item) {
+		return itemsDeCatalogo.contains(item);
+	}
+	public void eliminarItemDelCatalogo(Item item) {
+		this.verificarQueElItemEsteEnElCatalogo(item);
+		itemsDeCatalogo.remove(item);
+	}
+	public void verificarQueElItemEsteEnElCatalogo(Item item) {
+		if(!this.itemEstaEnElCatalogo(item)) {
+			throw new RuntimeException("El item"+item.nombre()+"no existe en el catalogo");
+		}
+	}
 }
