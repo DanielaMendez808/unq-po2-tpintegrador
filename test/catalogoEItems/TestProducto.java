9568d4292071a27805cc236a9f32d34d209e17d7
Daniela Mendez <danieladelcarmenmendez@gmail.com>
2026-07-01 23:02:30 -0300

    Nuevo

--- test/catalogoEItems/TestProducto.java
+++ test/catalogoEItems/TestProducto.java
@@ -2,16 +2,20 @@ package catalogoEItems;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Map;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import Exceptions.ErrorDeAtributoDinamico;
+import Exceptions.ErrorDeStockInsuficiente;
 import Exceptions.ErrorDeStringVacio;
 import gestionDePedido.Sucursal;
 
 class TestProducto {
 	private Producto cargador;
 	private Sucursal Ezpeleta;
+	private Sucursal Quilmes;
 	private AtributoDinamico voltaje;
 	private AtributoDinamico peso;
 	private AtributoDinamico atributoDinamicoConStringVacio;
@@ -23,6 +27,7 @@ class TestProducto {
 	voltaje= new AtributoDinamico("voltaje","10");
 	atributoDinamicoConStringVacio= new AtributoDinamico("","20");
 	Ezpeleta= new Sucursal("Sucursal Ezpeleta");
+	Quilmes= new Sucursal("Sucursal Quilmes");
 	cargador= new Producto("Cargador tipo C",
 			"Motorola",
 			"Electronica",
@@ -98,6 +103,65 @@ class TestProducto {
 			cargador.agregarAtributoDinamico(voltaje);
 	    });
 	}
+	@Test
+	void testProductoTieneStock() {
+		cargador.incrementarStock();
+		assertTrue(cargador.tieneStock());
+	}
+	@Test
+	void testProductoGetStock() {
+		cargador.incrementarStock();
+		cargador.incrementarStock();
+		assertEquals(2,cargador.getStock());
+	}
+	@Test
+	void testProductoSetStock() {
+		Map<Sucursal, Integer> stockDePrueba = Map.of(
+		        Quilmes, 15,
+		        Ezpeleta, 40
+		    );
+		cargador.setStock(stockDePrueba);
+		assertEquals(stockDePrueba, cargador.getDepósito());
+	}
+	@Test
+	void testProductoGetStockEnSucursal() {
+		Map<Sucursal, Integer> stockDePrueba = Map.of(
+		        Quilmes, 15,
+		        Ezpeleta, 40
+		    );
+		cargador.setStock(stockDePrueba);
+		assertEquals(40,cargador.getStockEnSucursal());
+	}
+	@Test
+	void testNoTieneStock() {
+		assertThrows(
+			    ErrorDeStockInsuficiente.class, () -> {
+			cargador.validarQueHayStockDelItem();
+	    });
+	}
+	@Test
+	void testFallaAlDecrementarStockPorqueNoTiene() {
+		assertThrows(
+			    ErrorDeStockInsuficiente.class, () -> {
+			cargador.decrementarStock();
+	    });
+	}
+	@Test
+	void testProductoGetMarca() {
+		assertEquals("Motorola",cargador.getMarca());
+	}
+	@Test
+	void testProductoSetMarca() {
+		cargador.setMarca("Samsung");
+		assertEquals("Samsung",cargador.getMarca());
+	}
+	@Test
+	void testProductoGetAtributosDinamicos() {
+		
+	}
+	
+	
+	
 	
 	
 
